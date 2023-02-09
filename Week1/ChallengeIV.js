78032dcb1df0b8d00666fefc85bba780e22988c5
Evgeny <todorovevgeny@gmail.com>
2023-02-09 17:49:43 -0500

    HTML for challenge VI and Javascript for challenge IV

---
+++ Week1/ChallengeIV.js
@@ -0,0 +1,11 @@
+/*
+    Created by: Evgeny Todorov
+    Last upated: 02/09/23
+    Description: Javascript array/list and function to add items to it.
+*/
+
+let yourHobbies = [];
+
+function addYourHobby (hobby) {
+    yourHobbies.push(hobby);
+}
